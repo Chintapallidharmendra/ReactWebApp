3b89457bf1b1914b654826ce3e7381cae8781986
Chintapalli Dharmendra <chintapallidharmendra@gmail.com>
2022-06-22 08:05:16 +0530

    Add timer example

--- src/components/useEffectExamples.js
+++ src/components/useEffectExamples.js
@@ -1,22 +1,61 @@
 import React, { useState, useEffect } from "react";
 import ContentTop from "./ContentTop";
 
+const timerTextClasses = {
+  textAlign: "center",
+  fontSize: "30px",
+  fontWeight: "bold",
+  fontStyle: "lato",
+};
+
+const TimerDisplay = () => {
+  const [timer, setTimer] = useState(0);
+  const tick = () => {
+    setTimer((prevTimer) => prevTimer + 1);
+  };
+  useEffect(() => {
+    const interval = setInterval(tick, 1000);
+    return () => {
+      console.log("timer cleared");
+      clearInterval(interval);
+    };
+  }, []);
+  return <div style={timerTextClasses}> {timer}</div>;
+};
+
+const MouseListener = () => {
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+
+  const logMousePosition = (e) => {
+    setMouseX(e.clientX);
+    setMouseY(e.clientY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", logMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", logMousePosition);
+    };
+  }, []);
+
+  return (
+    <>
+      <p>UseEffect- Mouse Listener </p>
+      <p>{`mouseX - ${mouseX} || mouseY - ${mouseY}`}</p>
+    </>
+  );
+};
+
 const UseEffectExamples = ({ handleGoToContentsClick }) => {
   const [name, setName] = useState("");
   const [count, setCount] = useState(0);
-  const [mouseX, setMouseX] = useState(0);
-  const [mouseY, setMouseY] = useState(0);
+  const [ismouLisActive, setMouLisStatus] = useState(false);
+  const [isTimerActive, setTimerStatus] = useState(false);
 
   useEffect(() => {
     document.title = `Clicked ${count} times`;
   }, [count]);
-
-  useEffect(() => {
-    window.addEventListener("mousemove", (e) => {
-      setMouseX(e.clientX);
-      setMouseY(e.clientY);
-    });
-  }, []);
 
   return (
     <div>
@@ -39,8 +78,24 @@ const UseEffectExamples = ({ handleGoToContentsClick }) => {
         Clicked {count} times
       </button>
       <hr />
-      <p>UseEffect to add event listener</p>
-      <p>{`mouseX - ${mouseX} || mouseY - ${mouseY}`}</p>
+      <button
+        onClick={() => {
+          setMouLisStatus(!ismouLisActive);
+        }}
+      >
+        Toggle Mouse Listener
+      </button>
+      {ismouLisActive && <MouseListener />}
+      <hr />
+      <button
+        onClick={() => {
+          setTimerStatus(!isTimerActive);
+        }}
+      >
+        Toggle Timer
+      </button>
+      {isTimerActive && <TimerDisplay />}
+      <hr />
     </div>
   );
 };
